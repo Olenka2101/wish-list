41da879247b470734761f8f2cc69851e9783655c
Olga Sessions <olenkafirst@gmail.com>
2025-06-19 13:59:01 -0400

    added button under each item

--- src/App.js
+++ src/App.js
@@ -3,9 +3,32 @@ import './App.css';
 import { data } from './Data';
 
 function App() {
+  const [gifts, setGifts] = useState(data);
+  console.log(data)
   return (
-    <div className="App">
-      
+    <div>
+    <div className="container">
+      <h1>List of {gifts.length} gifts</h1>
+    </div>
+
+       {gifts.map((element => {
+      const {id, gifty, image} = element;
+
+      return(<div key={id}>
+        <div className="container">
+        <h2> {id} - {gifty} </h2>
+        </div>
+        <div className="container">
+        <img src={image} width="300px" alt='various gifts'/>  
+        </div>
+        <div className="container">
+          <button>add</button>
+        </div>
+      </div>)
+    }))}
+     <div className="container">
+    <button onClick={()=> setGifts([])}>Delete all</button>
+    </div>
     </div>
   );
 }
